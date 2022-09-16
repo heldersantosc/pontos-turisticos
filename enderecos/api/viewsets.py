9744d847ed22af4aad25cecf73a23fb666bb157f
Helder Santos <heldersantos@bemol.com.br>
2022-09-16 18:57:20 -0400

    criacao do endpoint de comentario e correcao das viewsets
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from enderecos.api.serializers import EnderecoSerializer
 from enderecos.models import Endereco
 
-class EnderecosViewSet(viewsets.ModelViewSet):
+class EnderecoViewSet(viewsets.ModelViewSet):
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
