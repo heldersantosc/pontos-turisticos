9131ccf13becda1d4f60fa4e31797f29eb0668ce
Helder Santos <heldersantos@bemol.com.br>
2022-09-16 18:43:06 -0400

    criado endpoints de enderecos
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

---
+++ enderecos/api/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from enderecos.api.serializers import EnderecoSerializer
+from enderecos.models import Endereco
+
+class EnderecosViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
