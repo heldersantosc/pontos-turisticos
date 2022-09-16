a4af6e981939fa979878ca1e89bd2dd326412cf7
Helder Santos <heldersantos@bemol.com.br>
2022-09-16 19:11:35 -0400

    sobrescreve queryset para usar o get_queryset
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -3,5 +3,7 @@ from core.models import PontoTuristico
 from core.api.serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(viewsets.ModelViewSet):
-    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(aprovado=False)
