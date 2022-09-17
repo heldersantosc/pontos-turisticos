0cfc2a82fd4d2f857222513cc0077cf86b5fee51
Helder Santos <heldersantos@bemol.com.br>
2022-09-17 14:25:09 -0400

    implementa o uso de search filters
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- comentario/api/viewsets.py
+++ comentario/api/viewsets.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from comentario.api.serializers import ComentarioSerializer
 from comentario.models import Comentario
+from rest_framework import filters
+
 
 class ComentarioViewSet(viewsets.ModelViewSet):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["comentario"]
