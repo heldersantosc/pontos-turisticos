f237c172cce2fb003abe6457d927260bd0546735
Helder Santos <heldersantos@bemol.com.br>
2022-09-16 20:28:34 -0400

    implementa imagens estáticas para api
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- atracoes/models.py
+++ atracoes/models.py
@@ -1,12 +1,16 @@
-from django.db.models import Model, CharField, TextField, IntegerField
+from django.db import models
 
 # Create your models here.
-class Atracao(Model):
-    nome = CharField(max_length=150)
-    descricao = TextField()
-    horario_func = TextField()
-    idade_minima = IntegerField()
+class Atracao(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    horario_func = models.TextField()
+    idade_minima = models.IntegerField()
+    foto = models.ImageField(
+        upload_to="atracoes",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.nome
-
--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
@@ -9,13 +12,13 @@ from avaliacoes.api.viewsets import AvaliacaoViewSet
 from comentario.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
-router.register(r'pontosturisticos', PontoTuristicoViewSet, basename='pontoturistico')
-router.register(r'atracoes', AtracaoViewSet)
-router.register(r'enderecos', EnderecoViewSet)
-router.register(r'comentarios', ComentarioViewSet)
-router.register(r'avaliacoes', AvaliacaoViewSet)
+router.register(r"pontosturisticos", PontoTuristicoViewSet, basename="pontoturistico")
+router.register(r"atracoes", AtracaoViewSet)
+router.register(r"enderecos", EnderecoViewSet)
+router.register(r"comentarios", ComentarioViewSet)
+router.register(r"avaliacoes", AvaliacaoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-]
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
